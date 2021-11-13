196e478e2b1b39f3c4b2864cb91b55ea52644b9d
wai <42461097+wailu@users.noreply.github.com>
2021-11-14 00:42:07 +0800

    Update pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
-    <main>
-      <title>Home Page</title>
-      <h1>Welcome to my Gatsby site!</h1>
-      <Link to="/about">About</Link>
+    <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+        src="../images/treecko.jpeg"
+      />
+    </Layout>
   );
 };
 
