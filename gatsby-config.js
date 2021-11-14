941bc72180b4659d685b02b0e8fae8056b157992
wai <42461097+wailu@users.noreply.github.com>
2021-11-15 03:20:33 +0800

    Try data layer

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,16 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "My Gatsby Site",
+    title: "My First Gatsby Site",
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+  ],
 };
